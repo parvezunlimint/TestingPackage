6c25e064d78cd9e4b52ee75ea6c3625c53b38bbe
Mohammad Parvez <m.parvez@unlimint.com>
2023-11-09 16:50:16 +0530

    Package file location updated

--- Package.swift
+++ Package.swift
@@ -1,28 +1,40 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let MOYA_VERSION: PackageDescription.Version = "15.0.3"
+let ALAMOFIRE_VERSION: PackageDescription.Version = "5.6.4"
+
+let UNLIMINT_CORE_VERSION: PackageDescription.Version = "2.0.1"
+
 let package = Package(
-    name: "TestingPackage",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TestingPackage",
-            targets: ["TestingPackage"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TestingPackage",
-            dependencies: []),
-        .testTarget(
-            name: "TestingPackageTests",
-            dependencies: ["TestingPackage"]),
-    ]
+    
+  name: "UnlimintSDK_Core",
+  platforms: [.iOS(.v14)],
+  products: [
+    .library(
+      name: "UnlimintSDK_Core",
+      targets: ["UnlimintSDK_CoreA"]
+    )
+  ],
+  
+  dependencies: [
+    .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: MOYA_VERSION)),
+    .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: ALAMOFIRE_VERSION))
+  ],
+  
+  targets: [
+    
+    .target(
+      name: "UnlimintSDK_CoreA"
+    ),
+
+    .binaryTarget(
+      name: "UnlimintSDK_Core",
+      url: "https://github.com/parvezunlimint/TestingPackage/releases/download/0.0.1/UnlimintSDK-Core.zip",
+      checksum: "596e4ddc16154c78da7c5c4a6f47e9c6b93c7106"
+    )
+  ],
+  swiftLanguageVersions: [.v5]
 )

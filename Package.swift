88e3f77dc80b346b9d88cc100ddb997380af49fe
Mohammad Parvez <m.parvez@unlimint.com>
2023-11-10 11:55:08 +0530

    updated

--- Package.swift
+++ Package.swift
@@ -40,31 +40,30 @@ let UNLIMINT_CORE_VERSION: PackageDescription.Version = "2.0.1"
 //)
 
 
+
 let package = Package(
-    name: "MyLibrary",
+    name: "UnlimintSDK_CoreCheckout",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
-    
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["SomeRemoteBinaryPackage"])
-    ],
-    
-    dependencies: [
-            .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: MOYA_VERSION)),
-            .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: ALAMOFIRE_VERSION))
+            name: "UnlimintSDK_CoreCheckout",
+            targets: ["UnlimintSDK_CoreCheckout", "UnlimintSDK_Core"]
+        ),
     ],
     targets: [
-        
+        .target(
+            name: "UnlimintSDK_CoreCheckout",
+            path: "Sources/TestingPackage"
+        ),
         .binaryTarget(
-                name: "SomeRemoteBinaryPackage",
-                url: "https://github.com/parvezunlimint/TestingPackage/releases/download/1.0.0/UnlimintSDK-Core.zip",
-                checksum: "2b33a0035c53af54aed4d53667a4efeb942458ad2dcb418c1a6bf61bf36fe51d"
+                  name: "UnlimintSDK_Core",
+                  url: "https://github.com/parvezunlimint/TestingPackage/releases/download/1.0.0/UnlimintSDK-Core.zip",
+                  checksum: "2b33a0035c53af54aed4d53667a4efeb942458ad2dcb418c1a6bf61bf36fe51d"
             
-        )
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )

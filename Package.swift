203da9d872d804561c0c5c25af06aca5669dfee0
Mohammad Parvez <m.parvez@unlimint.com>
2023-11-20 17:57:47 +0530

    commit 2

--- Package.swift
+++ Package.swift
@@ -1,147 +1,15 @@
-// swift-tools-version: 5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.9
 
 import PackageDescription
 
+
+
 let SWINJECT_VERSION: PackageDescription.Version = "2.8.0"
 let MOYA_VERSION: PackageDescription.Version = "15.0.3"
-let MOTAMO_VERSION: PackageDescription.Version = "7.5.2"
 let ALAMOFIRE_VERSION: PackageDescription.Version = "5.6.4"
 
 let UNLIMINT_CORE_VERSION: PackageDescription.Version = "2.0.1"
 let UNLIMINT_SWIFTUI_VERSION: PackageDescription.Version = "2.0.1"
-
-//
-//let package = Package(
-//    name: "Unlimint_SwiftUI",
-//    platforms: [
-//        .iOS(.v14)
-//    ],
-//    products: [
-//        // Products define the executables and libraries a package produces, and make them visible to other packages.
-//       .library( name: "UnlimintSDKCore", targets: ["UnlimintSDKCoreTarget"]),
-////       .library( name: "UnlimintSDKSwiftUI", targets: ["UnlimintSDKSwiftUI1"]),
-//
-//    ],
-//    dependencies: [
-//        // Dependencies declare other packages that this package depends on.
-//        // .package(url: /* package url */, from: "1.0.0"),
-//
-//        .package(url: "https://github.com/Swinject/Swinject.git", from: SWINJECT_VERSION),
-//        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: MOYA_VERSION)),
-//        .package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", .upToNextMajor(from: MOTAMO_VERSION)),
-//        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from:ALAMOFIRE_VERSION))
-//
-//    ],
-//    targets: [
-//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//
-//        .binaryTarget(
-//                    name: "UnlimintSDKCoreBinary",
-//                    path: "./Sources/UnlimintSDK_Core.xcframework"),
-//
-////        .binaryTarget(name: "UnlimintSDK_SwiftUI",
-////                      path: "./Sources/UnlimintSDK_SwiftUI.xcframework"),
-//
-//
-//        .target(
-//            name: "UnlimintSDKCoreTarget",
-//            dependencies: ["Moya",
-//                           "Alamofire",
-//                           "matomo-sdk-ios",
-//                           "UnlimintSDKCoreBinary"]),
-//
-//
-////        .target(
-////            name: "UnlimintSDKSwiftUI1",
-////            dependencies: ["UnlimintSDK_Core", "Swinject", "Moya", "MatomoTracker"]),
-//    ]
-//)
-
-
-
-
-//let package = Package(
-//    name: "Unlimint_SwiftUI",
-//    platforms: [
-//        .iOS(.v14)
-//    ],
-//    products: [
-//        // Products define the executables and libraries a package produces, and make them visible to other packages.
-//       .library( name: "UnlimintSDKCore", targets: ["FirstFrameworkTargets"]),
-////       .library( name: "UnlimintSDKSwiftUI", targets: ["UnlimintSDKSwiftUI1"]),
-//
-//    ],
-//    dependencies: [
-//        // Dependencies declare other packages that this package depends on.
-//        // .package(url: /* package url */, from: "1.0.0"),
-//
-//        .package(url: "https://github.com/Swinject/Swinject.git", from: SWINJECT_VERSION),
-//        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: MOYA_VERSION)),
-//        .package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", .upToNextMajor(from: MOTAMO_VERSION))
-//
-//    ],
-//    targets: [
-//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-//        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//
-//        .binaryTarget(
-//                    name: "UnlimintSDK_Core",
-//                    url: "https://github.com/cardpay/ios-sdk-podspec/releases/download/2.0.1/UnlimintSDK_Core.zip",
-//                    checksum: "c4e46a2bb0f972a423bdae259c57f94f8b7a51d768a20d9acfd2aad7cbe8e898"),
-//
-//        .target(name: "FirstFrameworkTargets",
-//                        dependencies: [
-//                            .target(name: "UnlimintSDK_Core"),
-//                            .target(name: "Moya")
-//
-//                        ],
-//                    path: "FirstFrameworkTargets")
-//    ]
-//)
-
-
-
-//let package = Package(
-//
-//    name: "Unlimint_SwiftUI",
-//    platforms: [
-//        .iOS(.v13)
-//    ],
-//    products: [
-//        .library(
-//            name: "UnlimintSDK_Core",
-//            targets: ["UnlimintSDK_Core_Target"]
-//        )
-//    ],
-//
-//    dependencies: [
-//        // Dependencies declare other packages that this package depends on.
-//        // .package(url: /* package url */, from: "1.0.0"),
-//
-//        .package(url: "https://github.com/Swinject/Swinject.git", from: SWINJECT_VERSION),
-//        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: MOYA_VERSION)),
-//        .package(url: "https://github.com/matomo-org/matomo-sdk-ios.git", .upToNextMajor(from: MOTAMO_VERSION))
-//
-//    ],
-//
-//    targets: [
-//        .binaryTarget(
-//                    name: "UnlimintSDK_Core",
-//                    url: "https://github.com/cardpay/ios-sdk-podspec/releases/download/2.0.1/UnlimintSDK_Core.zip",
-//                    checksum: "c4e46a2bb0f972a423bdae259c57f94f8b7a51d768a20d9acfd2aad7cbe8e898"),
-//
-//        .target(name: "FrameworkCTargets",
-//                dependencies: [
-//                    .target(name: "UnlimintSDK_Core", condition: .when(platforms: .some([.iOS])))
-//                ],
-//                path: "FrameworkCTargets"
-//        )
-//    ],
-//    swiftLanguageVersions: [.v5]
-//)
-
 
 
 let package = Package(
@@ -152,6 +20,10 @@ let package = Package(
     .library(
       name: "UnlimintSDK_Core",
       targets: ["UnlimintSDK_CoreTarget"]
+    ),
+    .library(
+      name: "UnlimintSDK_SwiftUI",
+      targets: ["UnlimintSDK_SwiftUITarget"]
     )
   ],
   dependencies: [
@@ -173,6 +45,15 @@ let package = Package(
       path: "UnlimintSDK_Core"
     ),
 
+    
+    .target(
+        name: "UnlimintSDK_SwiftUITarget",
+        dependencies: [
+        .target(name: "UnlimintSDK_Core_Wrapper", condition: .when(platforms: [.iOS, .macCatalyst, .macOS, .tvOS])),
+        ],
+          path: "UnlimintSDK_SwiftUI"
+    ),
+    
     .target(
       name: "UnlimintSDK_Core_Wrapper",
       dependencies: [
@@ -210,7 +91,14 @@ let package = Package(
       name: "UnlimintSDK_Core",
       url: "https://github.com/parvezunlimint/TestingPackage/releases/download/1.0.1/UnlimintSDK-Core.xcframework.zip",
       checksum: "b2bef0998b9f8b71028289c686e54f6f018d82e16ae5734e592bd6fcedc86e7b"
+    ),
+    
+    .binaryTarget(
+      name: "UnlimintSDK_SwiftUI",
+      url: "https://github.com/parvezunlimint/TestingPackage/releases/download/1.0.1/UnlimintSDK-SwiftUI.xcframework.zip",
+      checksum: "bd0ee70f20d3aa95c8a934335bc2be18058d13818ab70420c8a54c144b23a56c"
     )
+    
   ],
   swiftLanguageVersions: [.v5]
 )
